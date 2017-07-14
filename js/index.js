b7a0de378829fed770a3f3f5b9f418e92f39cd9a
李家太 <2862260725@qq.com>
2017-07-14 14:30:39 +0800

    7-14-14

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 window.onload = function() {
+    var index = 1;
+    var animated = false;
+    var interval = 3000;
     var wrap = document.getElementById('wrap');
     var list = document.getElementById('list');
     var buttons = document.getElementById('buttons').getElementsByTagName('span');
     var prev = document.getElementById('prev');
     var next = document.getElementById('next');
-    var index = 1;
-    var animated = false;
-    var interval = 3000;
-    var timer;
+    console.log(buttons);
 
+    //动画封装函数
     function animate(offset) {
         if (offset == 0) {
             return;
@@ -18,24 +19,26 @@ window.onload = function() {
         var inteval = 50;
         var speed = offset/(time/inteval);
         var left = parseInt(list.style.left) + offset;
-        var go = function (){
+
+        var go = function() {
             if ( (speed > 0 && parseInt(list.style.left) < left) || (speed < 0 && parseInt(list.style.left) > left)) {
                 list.style.left = parseInt(list.style.left) + speed + 'px';
-                setTimeout(go, inteval);
+                setTimeout(go,inteval);
             } else {
                 list.style.left = left + 'px';
-                if (left > -200) {
-                    list.style.left = '-3000px';
+                if (left > -600) {
+                    list.style.left = -3000 + 'px';
                 }
                 if (left < -3000) {
-                    list.style.left = '-600px';
+                    list.style.left = -600 + 'px';
                 }
                 animated = false;
+                console.log(animated);
             }
-        } 
+        }
         go();
     }
-
+    //焦点动画
     function showButton() {
         for (var i = 0; i < buttons.length; i++) {
             if (buttons[i].className == 'on') {
@@ -45,28 +48,38 @@ window.onload = function() {
         }
         buttons[index-1].className = 'on';
     }
-    function play() {
-        timer = setTimeout(function() {
+    //焦点点击事件
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = function(){
+            if (animated) {
+                return;
+            }
+            if (this.className == 'on') {
+                return;
+            }
+            var myIndex = parseInt(this.getAttribute('index'));
+            var offset = -600*(myIndex-index);
+            animate(offset);
+            index = myIndex;
+            showButton();
+
+        }
+    }
+    //自动播放与停止播放
+    function play(){
+        timer = setTimeout(function(){
             prev.onclick();
             play();
-        },interval);
+        },interval)
     }
-    function stop() {
+    play();
+    function stop(){
         clearTimeout(timer);
     }
-    next.onclick = function() {
-        if (animated) {
-            return;
-        }
-        if (index == 1) {
-            index = 5;
-        } else {
-            index -= 1;
-        }
-        animate(600);
-        showButton();
-    }
-    prev.onclick = function() {
+    wrap.onmouseover = stop;
+    wrap.onmouseout = play;
+    //左箭头
+    prev.onclick = function(){
         if (animated) {
             return;
         }
@@ -78,22 +91,17 @@ window.onload = function() {
         animate(-600);
         showButton();
     }
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].onclick = function() {
-            if (animated) {
-                return;
-            }
-            if (this.className == 'on') {
-                return;
-            }
-            var myIndex = parseInt(this.getAttribute('index'));
-            var offset = -600*(myIndex - index);
-            animate(offset);
-            index = myIndex;
-            showButton();
+    //右箭头
+    next.onclick = function(){
+        if (animated) {
+            return;
         }
+        if (index == 1) {
+            index = 5;
+        } else {
+            index -= 1;
+        }
+        animate(600);
+        showButton();
     }
-    wrap.onmouseover = stop;
-    wrap.onmouseout = play;
-    play();
 }
